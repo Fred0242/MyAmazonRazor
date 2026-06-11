c618f519b3ceb6c7dc569e59f341c10c1a0df4cd
Fred0242 <itachimadzou@gmail.com>
2026-06-11 12:37:41 +0200

    Initial commit: Razor Pages project setup

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyAmazonRazor.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
